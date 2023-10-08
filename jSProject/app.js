a6bca6d6b4b14077eba3c2d0c43ef3d118517412
wendileee <144676811+wendileee@users.noreply.github.com>
2023-10-08 21:49:43 +0100

    Create app.js
    remove unwanted code

---
+++ jSProject/app.js
@@ -0,0 +1,14 @@
+const colors=["green","red","rgba(133,122,200)", "f15025"];
+const btn=document.getElementById("btn");
+const color=document.querySelector(".color");
+
+btn.addEvenetListener("click",functin(){
+  //get random number between 0-3 colors[3]
+const randomNumber = getRandomNumeber();
+document.body.style.backgroundColor = colors[randomNumber];
+color.textContent= colors[randomNumber];
+});
+
+function getRandomNumber(){
+return MathFloor(Math.random()*colors.length);
+}
