b2ee839a8317d7f264bdbd106dc3e93fbb00179d
wendileee <144676811+wendileee@users.noreply.github.com>
2023-10-12 00:34:57 +0100

    Update app.js
    fix click button by correcting syntax error function()

--- jSProject/app.js
+++ jSProject/app.js
@@ -2,7 +2,7 @@ const colors=["green","red","rgba(133,122,200)", "f15025"];
 const btn=document.getElementById("btn");
 const color=document.querySelector(".color");
 
-btn.addEvenetListener("click",functin(){
+btn.addEvenetListener("click",function(){
   //get random number between 0-3 colors[3]
 const randomNumber = getRandomNumeber();
 document.body.style.backgroundColor = colors[randomNumber];
