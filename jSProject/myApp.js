f9ab5e8870b34420b350c13234d4d9f2efe60c5a
wendileee <144676811+wendileee@users.noreply.github.com>
2023-11-16 09:47:27 +0000

    Create myApp.js
    learning basic node js

---
+++ jSProject/myApp.js
@@ -0,0 +1,41 @@
+let express = require('express');
+let app = express();
+
+app.get("/", function(req, res)
+        {res.send('Hello Express');}); 
+        
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("Hello World");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
