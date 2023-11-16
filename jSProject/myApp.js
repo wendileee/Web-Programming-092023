0d2c1337ae70596a3b2f6ca0cd5517bef7772691
wendileee <144676811+wendileee@users.noreply.github.com>
2023-11-16 10:00:23 +0000

    Update myApp.js
    serve html file using the res.sendFile syntax

--- jSProject/myApp.js
+++ jSProject/myApp.js
@@ -1,9 +1,11 @@
 let express = require('express');
 let app = express();
 
-app.get("/", function(req, res)
-        {res.send('Hello Express');}); 
-        
+//app.get("/", function(req, res)
+  //      {res.send('Hello Express');}); 
+   app.get("/",function (req, res){
+           res.sendFile(__dirname + "views/index.html");
+   });
 
 
 
