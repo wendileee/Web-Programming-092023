0ecb12ab9d579b6429208a0cc252e20f959713fb
wendileee <144676811+wendileee@users.noreply.github.com>
2023-11-20 20:33:59 +0000

    Update myApp.js
    storing static assets in html server as well as serving json on specific route using the app.use function and res.json() syntax

--- jSProject/myApp.js
+++ jSProject/myApp.js
@@ -6,6 +6,11 @@ let app = express();
    app.get("/",function (req, res){
            res.sendFile(__dirname + "views/index.html");
    });
+app.use('/public',express.static(__dirname+'/public'));
+
+app.get("/json",function(req,res){
+   res.json({"message":"Hello json"});              
+});
 
 
 
