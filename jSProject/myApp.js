9e260a5ee6345042badf03149cb0263e4480b471
wendileee <144676811+wendileee@users.noreply.github.com>
2023-11-20 21:51:36 +0000

    Update myApp.js
    implemented env file

--- jSProject/myApp.js
+++ jSProject/myApp.js
@@ -8,8 +8,15 @@ let app = express();
    });
 app.use('/public',express.static(__dirname+'/public'));
 
-app.get("/json",function(req,res){
-   res.json({"message":"Hello json"});              
+//app.get("/json",function(req,res){
+  // res.json({"message":"Hello json"});              
+//});
+app.get("/json", function(req, res) {
+  if ( process.env["MESSAGE_STYLE"] == "uppercase"){
+    res.json({"message":"HELLO JSON"})
+  }else{
+    res.json({"message":"Hello json"})
+  }
 });
 
 
